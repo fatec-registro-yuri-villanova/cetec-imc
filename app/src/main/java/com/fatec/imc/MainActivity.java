3bd9a939c79e260cd2bf4bc96fe0d4e4df33ed4f
yvillan <114770837+yvillan@users.noreply.github.com>
2024-10-13 09:10:59 -0300

    feat: add statusbar spacer

--- app/src/main/java/com/fatec/imc/MainActivity.java
+++ app/src/main/java/com/fatec/imc/MainActivity.java
@@ -9,6 +9,9 @@ import android.widget.RadioGroup;
 import android.widget.TextView;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -24,6 +27,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
         editTextPeso = findViewById(R.id.editTextPeso);
         editTextAltura = findViewById(R.id.editTextAltura);
         radioGroupSexo = findViewById(R.id.radioGroupSexo);
